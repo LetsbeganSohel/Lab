85c38eea3f883e3f2ba330c1d314fcbe82c521d2
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-12 20:55:43 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+//Write a Java programe to print the sum(addition), multy,substract,divide and reminder of two numbers.
+
+
+class Calculator{
+public static void main(String[] args){
+int a=125;
+int b=24;
+int sum,multy,sub,remd;
+double divi;
+sum=a+b;
+multy=a*b;
+sub= a-b;
+divi=a/b;
+remd=a%b;
+System.out.println("The Addition is :"+ sum);
+System.out.println("The Subtraction is :"+ sub);
+System.out.println("The Multiplication  is :"+ multy);
+System.out.println("The Divition is :"+ divi);
+System.out.println("The Reminder is :"+ remd);
+}}

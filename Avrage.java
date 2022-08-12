5b05758741afe38c31891d612e4b714472bba853
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-12 20:52:36 +0530

    Add files via upload

---
+++ Avrage.java
@@ -0,0 +1,15 @@
+//Write a Java programe that takes five numbers as input to cal;culate and print the average of the number
+
+
+
+class Avrage{
+public static void main(String [] args){
+int a,b,c,d,e;
+a=10;
+b=20;
+c=30;
+d=40;
+e=50;
+int avg=(a+b+c+d+e)/5;
+System.out.println("The Average is :"+avg);
+}}

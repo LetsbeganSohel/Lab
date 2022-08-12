d2dec56080911bc625ef969d71bbc4a1b69a2457
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-12 20:55:15 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+//Swapping two variable refers to manully exchange th valus of the variable. Generally this is done in moemory
+
+
+class Swapping{
+public static void main(String[] args){
+int a,b,temp;
+a=30;
+b=40;
+System.out.println(a + " " +b);
+System.out.println("After swapping");
+temp=a;
+a=b;
+b=temp;
+System.out.println(a + " " +b);
+}
+}

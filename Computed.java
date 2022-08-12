3ad8397aa754fbfb821d718f7faf7eafec7239f1
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-12 20:54:55 +0530

    Add files via upload

---
+++ Computed.java
@@ -0,0 +1,16 @@
+//Write a Java programe ton compute the specified expression and print the output.
+
+
+
+
+class Computed{
+public static void main(String[] args){
+double  a,b,c,d;
+a=25.5;
+b=3.5;
+c=40.5;
+d=4.5;
+double F;
+F=((a*b)-(b*b))/(c-d);
+System.out.println("The answer is :"+F);
+}}

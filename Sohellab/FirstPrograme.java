00fe8bb0f7be24fbc6e9ec3d16199fa957f00ce8
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-19 11:26:58 +0530

    Add files via upload

---
+++ Sohellab/FirstPrograme.java
@@ -0,0 +1,24 @@
+//
+package com.company;
+
+public class FirstPrograme {
+
+	public static void main(String[] args) {
+		int a=101;
+		int b=3;
+		int cal=(a+0)/3;
+		System.out.println(cal);
+		double d=3.0;
+		int n=6;
+		double m=(d-6)*10000000.1;
+		System.out.println(m);
+		System.out.println(true &&true);
+		System.out.println(false && true);
+		System.out.println((false && false) || (true && true));
+		System.out.println((false || false) && (true && true));
+
+	}
+		
+}
+  
+

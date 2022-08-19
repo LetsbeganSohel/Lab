2363c7952b09d2035565e361f458195aeb1d3f1d
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-08-19 11:29:53 +0530

    Add files via upload

---
+++ Sohellab/Fortrhprograme.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Fortrhprograme {
+
+	public static void main(String[] args) {
+		int a=5467874;
+		System.out.println(a);
+		System.out.print(5 +" " + 4 +" " +6 +" " +7 +" " +8 + " " +7 + " " +4);
+	}
+
+}
